fc47f15bf0811afac93603db1edf79abf7e49fad
JazzXHiro <73639405+JazzXHiro@users.noreply.github.com>
2024-07-03 13:24:48 +0530

    Displayed player and incorporated basic player movement

---
+++ code/entities.py
@@ -0,0 +1,29 @@
+from pygame.sprite import Group
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.Surface((100,100))
+        self.image.fill("red")
+        self.rect = self.image.get_frect(center = pos)
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+        input_vector = vector()
+        if keys[pygame.K_w]:
+            input_vector.y -= 1
+        if keys[pygame.K_s]:
+            input_vector.y += 1
+        if keys[pygame.K_a]:
+            input_vector.x -= 1
+        if keys[pygame.K_d]:
+            input_vector.x += 1
+        self.direction = input_vector           
+    def move(self, dt):
+        self.rect.center += self.direction * 250 * dt
+    
+    def update(self, dt):
+        self.input()
+        self.move(dt)
+            
--- code/main.py
+++ code/main.py
@@ -1,32 +1,49 @@
 from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
+from sprites import Sprite
+from entities import Player
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Jaded')
+        self.clock = pygame.time.Clock()
+        
+        # Groups
+        self.all_sprites = pygame.sprite.Group()
         
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'house')
         
     def import_assets(self):
         self.tmx_maps = {'world' : load_pygame(join('data', 'maps', 'world.tmx'))}
-        
+        print("Assets imported")
+
     def setup(self, tmx_map, player_start_pos):
         for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            print(x*TILE_SIZE, y*TILE_SIZE, surf)
+            Sprite((x*TILE_SIZE, y*TILE_SIZE), surf, self.all_sprites)
         
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
+                Player((obj.x, obj.y), self.all_sprites)
+                print(f"Player created at position: ({obj.x}, {obj.y})")
+
     def run(self):
         while True:
-            #event loop
+            dt = self.clock.tick() / 1000 #gives us the time difference b/w the last frame and the current frame
+            # Event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
                     
-            #game logic
+            # Game logic
+            self.all_sprites.update(dt)
+            self.all_sprites.draw(self.display_surface)
+            print(self.clock.get_fps())
+            print(dt)
             pygame.display.update()
             
 if __name__ == '__main__':
