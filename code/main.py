eb7b4cae9f8ff1ceefca5537fc25333380eb9679
JazzXHiro <73639405+JazzXHiro@users.noreply.github.com>
2024-07-15 17:35:28 +0530

    added the character sprite and animation

--- code/main.py
+++ code/main.py
@@ -25,39 +25,41 @@ class Game:
 		self.tmx_maps = {
 			'world': load_pygame(join('data', 'maps', 'world.tmx')),
 			'hospital': load_pygame(join('data', 'maps', 'hospital.tmx')),
-			}
+		}
 
 		self.overworld_frames = {
 			'water': import_folder('graphics', 'tilesets', 'water'),
-			'coast': coast_importer(24, 12, 'graphics', 'tilesets', 'coast')
+			'coast': coast_importer(24, 12, 'graphics', 'tilesets', 'coast'),
+			'characters': all_character_import('graphics', 'characters')
 		}
+		print(self.overworld_frames['characters'])
 	
 	def setup(self, tmx_map, player_start_pos):
-		# terrain
-		for layer in ['Terrain', 'Terrain Top']:
-			for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
-				Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+			# terrain
+			for layer in ['Terrain', 'Terrain Top']:
+				for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+					Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
-		# objects 
-		for obj in tmx_map.get_layer_by_name('Objects'):
-			Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+			# objects 
+			for obj in tmx_map.get_layer_by_name('Objects'):
+				Sprite((obj.x, obj.y), obj.image, self.all_sprites)
 
-		# entities 
-		for obj in tmx_map.get_layer_by_name('Entities'):
-			if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
-				  self.player = Player((obj.x, obj.y), self.all_sprites)
+			# entities 
+			for obj in tmx_map.get_layer_by_name('Entities'):
+				if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
+					self.player = Player(pos = (obj.x, obj.y), frames = self.overworld_frames['characters']['player'], groups = self.all_sprites)
 
-		# water 
-		for obj in tmx_map.get_layer_by_name('Water'):
-			for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
-				for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
-					AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites)
+			# water 
+			for obj in tmx_map.get_layer_by_name('Water'):
+				for x in range(int(obj.x), int(obj.x + obj.width), TILE_SIZE):
+					for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+						AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites)
 
-		# coast
-		for obj in tmx_map.get_layer_by_name('Coast'):
-			terrain = obj.properties['terrain']
-			side = obj.properties['side']
-			AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites)
+			# coast
+			for obj in tmx_map.get_layer_by_name('Coast'):
+				terrain = obj.properties['terrain']
+				side = obj.properties['side']
+				AnimatedSprite((obj.x, obj.y), self.overworld_frames['coast'][terrain][side], self.all_sprites)
 
 	def run(self):
 		while True:
