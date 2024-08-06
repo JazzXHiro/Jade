b5a66d761fa6327ee3924dfac4b7edad4f968dcd
JazzXHiro <73639405+JazzXHiro@users.noreply.github.com>
2024-08-06 13:39:55 +0530

    added the functionality of cycling through dialogues

--- code/main.py
+++ code/main.py
@@ -24,6 +24,8 @@ class Game:
 
 		self.import_assets()
 		self.setup(self.tmx_maps['world'], 'house')
+		
+		self.dialog_tree = None
 
 	def import_assets(self):
 		self.tmx_maps = {
@@ -87,16 +89,18 @@ class Game:
                    	character_data = TRAINER_DATA[obj.properties['character_id']])
 
 	def input(self):
-		keys = pygame.key.get_just_pressed()
-		if keys[pygame.K_SPACE]:
-			for character in self.character_sprites:
-				if check_connection(100, self.player, character): #radius,
-					self.player.block() #block player input
-					character.change_facing_direction(self.player.rect.center)#entities face each other
-					self.create_dialog(character)
+		if not self.dialog_tree:
+			keys = pygame.key.get_just_pressed()
+			if keys[pygame.K_SPACE]:
+				for character in self.character_sprites:
+					if check_connection(100, self.player, character): #radius,
+						self.player.block() #block player input
+						character.change_facing_direction(self.player.rect.center)#entities face each other
+						self.create_dialog(character)
      
 	def create_dialog(self, character):
-		DialogTree(character, self.player, self.all_sprites, self.fonts['dialog'])
+		if not self.dialog_tree:
+			self.dialog_tree = DialogTree(character, self.player, self.all_sprites, self.fonts['dialog'])
 
 	def run(self):
 		while True:
@@ -113,6 +117,9 @@ class Game:
 			self.display_surface.fill('black')
 			self.all_sprites.draw(self.player.rect.center)
 			pygame.display.update()
+   
+			#overlays
+			if self.dialog_tree: self.dialog_tree.update()
 
 if __name__ == '__main__':
 		game = Game()
